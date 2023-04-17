62464f8f3179dec0cff0f50f8e4982d99b0a7b5e
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-04-17 14:52:00 -0400

    hi

---
+++ grade.js
@@ -0,0 +1,17 @@
+
+let mygrade = 54
+if(mygrade <= 100 && mygrade >= 90){
+    console.log("You Grade is A")
+}
+if(mygrade <= 89 && mygrade >= 80){
+    console.log("You Grade is B")
+    }
+if(mygrade <= 79 && mygrade >= 70){
+        console.log("You Grade is C")
+        }
+if(mygrade <= 69 && mygrade >= 60){
+console.log("You Grade is D")
+ }
+ if(mygrade <= 59 && mygrade >= 0){
+    console.log("You Grade is F")
+ }
