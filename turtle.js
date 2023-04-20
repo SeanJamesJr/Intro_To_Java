32d8751c4871aa52a55567ab352532d78dcadb41
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-04-20 14:03:47 -0400

    hi

--- turtle.js
+++ turtle.js
@@ -22,8 +22,11 @@ turtle.setFillStyle('blue')
 //turtle.right(90)
 //}
 
+turtle.setSpeed(0.9)
+
 turtle.setPosition(50,140)
 
+turtle.setFillStyle("cyan")
 turtle.beginPath()
 for (let i=0; i < 4; i++) {
     console.log(i)
@@ -31,7 +34,9 @@ for (let i=0; i < 4; i++) {
     turtle.right(90)
     }
     turtle.closePath()
+    turtle.fill('cyan')
 
+turtle.setFillStyle("cyan")
 turtle.beginPath()
 
 turtle.forward(75)
@@ -41,12 +46,55 @@ turtle.right(90)
 turtle.forward(55)
 turtle.right(135)
 turtle.forward(75)
+turtle.closePath()
+turtle.fill()
 
+
+
+turtle.setPosition(80,120)
+turtle.beginPath()
+turtle.right(270)
+turtle.forward(20)
+turtle.right(268)
+turtle.forward(20)
+turtle.right(270)
+turtle.forward(20)
+turtle.right(270)
+turtle.forward(20)
 turtle.closePath()
 
+turtle.setPosition(50,75)
 turtle.beginPath()
-turtle.right(278)
-turtle.forward()
+turtle.right(180)
+turtle.forward(20)
+turtle.right(180)
+turtle.right(270)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(20)
+turtle.closePath()
+turtle.setPosition(125,75)
+
+turtle.beginPath()
+turtle.right(273)
+turtle.forward(20)
+turtle.right(270)
+turtle.forward(20)
+turtle.right(270)
+turtle.forward(20)
+turtle.right(270)
+turtle.forward(20)
+turtle.closePath()
+
+
+
+
+
+
+
+
 
 
 
