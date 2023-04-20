9729095ded10b0ab3627394394c60ee5ba5f44a0
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-04-20 14:53:05 -0400

    hi

---
+++ turtle2.js
@@ -0,0 +1,19 @@
+let aquarium = document.getElementById("aquarium")
+
+
+let turtle = new RealTurtle.default(aquarium,{autoStart: true})
+
+
+function polygon(sides,size=20) {
+
+    for (let i=0; i<sides; i++){
+        turtle.forward(size)
+        turtle.right(360/sides)
+    }
+
+}
+
+polygon(8)
+
+for (let i=0; i < 5; i++);
+polygon((Math.random()*10)+3)
