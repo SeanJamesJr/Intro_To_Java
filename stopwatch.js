54027c0247725cbb405e49c740a32eca12c8cbd5
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-04-28 14:51:38 -0400

    hi

---
+++ stopwatch.js
@@ -0,0 +1,14 @@
+function start() {
+
+    alert("You clicked the start button")
+}
+
+function stop(){
+
+    alert("You clicked the stop button")
+}
+
+function reset(){
+
+    alert("You clicked the restart button")
+}
