66936cd9f271125aac0b6e9d14cd357a81c0934a
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-05-01 14:17:59 -0400

    hi

--- stopwatch.js
+++ stopwatch.js
@@ -1,12 +1,35 @@
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+
+
+let digits = document.getElementById('digits')
+
 function start() {
 
-    alert("You clicked the start button")
+    interval=setInterval ( function() {
+        milliseconds = milliseconds + 10
+    
+    if (milliseconds >= 1000) {
+        seconds = seconds + 1
+        milliseconds = 0
+    }
+    
+    if (seconds>= 60) {
+        minutes = minutes + 1
+        seconds = 0
+    }
+    digits.innerHTML= minutes + ':' + String(seconds).padStart(2,'0') + ':' + milliseconds
+    console.log(minutes)
+    console.log(seconds)
+    console.log(milliseconds)
+    },10)
+
 }
 
-function stop(){
-
-    alert("You clicked the stop button")
-}
+    function stop (){
+        clearInterval(interval)
+        }
 
 function reset(){
 
